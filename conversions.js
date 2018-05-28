949199ac15971008e24988078b3d96d4292a6d4e
Luis Canales <kopz9999@gmail.com>
2018-05-28 15:03:05 -0700

    add conversions script

--- conversions.js
+++ conversions.js
@@ -13,8 +13,23 @@
     });
   }
 
+  function locationConversion(paths, conversionId) {
+    if (paths.includes(window.location.pathname)) {
+      gtag('event', 'conversion', {'send_to': conversionId });
+    }
+  }
+
   jQuery(document).ready(function() {
     clientSignInConversion();
     completedLeadAssessmentTestConversion();
+
+    // CA - Full Conversion - Start and Complete Test
+    locationConversion('/clients', 'AW-880579024/ajw1CJbhr4MBENCj8qMD');
+    // CA - Lead Thankyou
+    locationConversion('/thankyou', 'AW-880579024/F__CCKiIqYMBENCj8qMD');
+    // CA - Leads - Test Takers at Create Account Page
+    locationConversion('/clients/assessment/welcome', 'AW-880579024/HoF-CK2KqYMBENCj8qMD');
+    // CA - Metabolic Profile Test
+    locationConversion('/register/goals', 'AW-880579024/psM1COOEmYMBENCj8qMD');    
   });
 })(jQuery);
